69edd705113c1cbd6ec2a514cef87746ba440d86
Nguyen Minh Tam <23521390@gm.uit.edu.vn>
2025-10-28 12:18:36 +0700

    tạo folder fe và be

---
+++ frontend/.next/dev/server/chunks/07e91_next_headers_574a7ef9.js
@@ -0,0 +1,12 @@
+module.exports = [
+"[project]/my-app/node_modules/next/headers.js [middleware] (ecmascript, async loader)", ((__turbopack_context__) => {
+
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "server/chunks/07e91_next_9357c783._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[project]/my-app/node_modules/next/headers.js [middleware] (ecmascript)");
+    });
+});
+}),
+];
