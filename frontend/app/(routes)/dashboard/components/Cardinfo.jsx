a23be7f8c0cd7195bf55bddab00671d5e5d0d5bd
nguyenvanthanh <nguyenvanthanh1501bmt@gmail.com>
2025-12-11 11:19:32 +0700

    Xóa các thứ không cần thiết

--- frontend/app/(routes)/dashboard/components/Cardinfo.jsx
+++
@@ -1,4 +0,0 @@
-'use client'
-
-import React, {useState, useEffect }  from "react"
-import Sidenav from ''
--- frontend/app/(routes)/dashboard/loading.jsx
+++ frontend/app/(routes)/dashboard/loading.jsx
@@ -4,7 +4,7 @@ export default function Loading() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-50">
       {/* Spinner đơn giản */}
-      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-600 border-gray-200 mb-4"></div>
+      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-600 mb-4"></div>
       
       {/* Text loading */}
       <p className="text-gray-700 text-lg font-medium">Đang tải dashboard...</p>
